745ce0a666210f7a392ad903aae37351b131210d
Domenic Presta <domenic.prestia@mastt.com>
2026-02-24 15:37:17 +1100

    fix neo-tree opening on gD

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -16,5 +16,41 @@ return {
         end)
       end,
     })
+
+    vim.api.nvim_create_autocmd('VimEnter', {
+      once = true,
+      callback = function()
+        vim.api.nvim_create_autocmd('BufEnter', {
+          callback = function(ev)
+            local ft = vim.bo[ev.buf].filetype
+            local buftype = vim.bo[ev.buf].buftype
+            if ft == 'neo-tree' or buftype ~= '' then
+              return
+            end
+            vim.schedule(function()
+              vim.cmd 'Neotree show reveal'
+            end)
+          end,
+        })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('QuitPre', {
+      callback = function()
+        local wins = vim.api.nvim_list_wins()
+        local real_wins = 0
+        for _, w in ipairs(wins) do
+          local buf = vim.api.nvim_win_get_buf(w)
+          local ft = vim.bo[buf].filetype
+          local is_floating = vim.api.nvim_win_get_config(w).relative ~= ''
+          if ft ~= 'neo-tree' and not is_floating then
+            real_wins = real_wins + 1
+          end
+        end
+        if real_wins == 1 then
+          vim.cmd 'Neotree close'
+        end
+      end,
+    })
   end,
 }
