168e1a8fb54a1d2b9254fdbdd3e814404064e1f9
Domenic Presta <domenic.prestia@mastt.com>
2026-03-10 13:58:54 +1100

    remove horrible claude code attempt, add opencode plugin with default settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,15 @@ return {
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
   init = function()
     vim.api.nvim_create_autocmd('BufAdd', {
       callback = function()
