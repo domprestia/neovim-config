c25809c31e449fa1d4450122239e89543aaa66e1
Domenic Presta <domenic.prestia@mastt.com>
2026-03-11 12:18:11 +1100

    reduce scroll speed so ghostty stops freaking out

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ vim.opt.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+-- Adjust scroll speed for smoother scrolling in terminal emulators like Ghostty
+vim.opt.mousescroll = 'ver:1,hor:1'
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
