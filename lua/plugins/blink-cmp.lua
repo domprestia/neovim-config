faee122d20dcdb8aaa5847f2f2d64b535c1e5f4f
Domenic Presta <domenic.prestia@mastt.com>
2026-03-17 10:09:14 +1100

    install blink

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,40 @@
+-- Autocompletion
+return {
+  'Saghen/blink.cmp',
+  version = '*',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
+  opts = {
+    keymap = {
+      preset = 'default',
+      -- Accept suggestion with C-y (mirrors old nvim-cmp binding)
+      ['<C-y>'] = { 'accept', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<C-space>'] = { 'show', 'fallback' },
+      -- Avoid Tab conflict with minuet virtual text
+      ['<Tab>'] = {},
+      ['<S-Tab>'] = {},
+    },
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = 'mono',
+    },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+      providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          score_offset = 100,
+        },
+      },
+    },
+    completion = {
+      trigger = { prefetch_on_insert = false },
+    },
+  },
+}
