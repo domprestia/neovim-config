d6103a54e3c6762fecefa129f8875a2cf10835a3
Domenic Presta <domenic.prestia@mastt.com>
2026-05-07 10:02:17 +1000

    always prefer path aliases over relative paths for imports

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -8,5 +8,12 @@ return {
       client.server_capabilities.documentFormattingProvider = false
       client.server_capabilities.documentRangeFormattingProvider = false
     end,
+    settings = {
+      tsserver_file_preferences = {
+        -- Always prefer path aliases (e.g. `@/lib/foo`) over relative paths (`../../lib/foo`)
+        -- on auto-imports. Falls back to relative when no alias matches.
+        importModuleSpecifierPreference = 'non-relative',
+      },
+    },
   },
 }
